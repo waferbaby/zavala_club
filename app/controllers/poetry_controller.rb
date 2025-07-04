8cd95a7901c4a9ecb0b535823a81193d844234f4
Daniel Bogan <d@waferbaby.com>
2025-07-04 10:48:34 +0000

    Add the error handler and fallback poem

--- app/controllers/poetry_controller.rb
+++ app/controllers/poetry_controller.rb
@@ -1,9 +1,17 @@
 class PoetryController < ApplicationController
+  FAIL_POEM = "it seems i've failed you\n and can't show you a poem\n  but i still love you".freeze
+
   def index
     @poem = Poem.generate
+    render :show
   end
 
   def show
-    @poem = Poem.find_by(id: params[:id])
+    @poem = Poem.find_by(id: params[:id]) || FAIL_POEM
+  end
+
+  def error
+    @poem = FAIL_POEM
+    render :show
   end
 end
