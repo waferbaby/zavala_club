3a3b620f90cbc2c7a2ff096545a255a267d547b0
Daniel Bogan <d@waferbaby.com>
2025-07-04 08:25:13 +0000

    Add the generate method

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,27 @@
 class Poem < ApplicationRecord
+  enum :form, [ :haiku, :limerick ]
+
+  def self.generate(form = :haiku)
+    options = { proper: false }
+
+    case form
+    when :haiku
+      options[:indent] = "012"
+    end
+
+    options[:form] = form
+
+    contents = Poefy::Poem.new(database_path, local: true).poem(options).join("\n")
+    return nil unless contents.length > 0
+
+    create!(form: form, contents: contents)
+  end
+
+  def to_s
+    contents.downcase
+  end
+
+  def self.database_path
+    Rails.root.join("storage", "corpus.sqlite3")
+  end
 end
