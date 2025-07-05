9582c925975b0bd5541879ddc8fccccdd01afb02
Daniel Bogan <d@waferbaby.com>
2025-07-05 06:40:46 +0000

    Update the name, remove the error handler

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,17 +1,10 @@
-class PoetryController < ApplicationController
-  FAIL_POEM = "it seems i've failed you\n and can't show you a poem\n  but i still love you".freeze
-
-  def index
+class PoemsController < ApplicationController
+  def create
     @poem = Poem.generate
     render :show
   end
 
   def show
-    @poem = Poem.find_by(id: params[:id]) || FAIL_POEM
-  end
-
-  def error
-    @poem = FAIL_POEM
-    render :show
+    @poem = Poem.find_by!(digest: params[:id])
   end
 end
