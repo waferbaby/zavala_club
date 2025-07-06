afc66af9dc863d6414489cfb13270b64dee548cf
Daniel Bogan <d@waferbaby.com>
2025-07-06 10:36:38 +0000

    Add plain text and JSON flavours too

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -8,10 +8,10 @@ class PoemsController < ApplicationController
     @poem = Poem.find_by!(digest: params[:id])
 
     respond_to do |format|
-      format.html { }
-      format.jpeg do
-        send_data poem_preview_data(@poem), type: "image/jpeg", disposition: "inline"
-      end
+      format.html
+      format.text { render plain: @poem.to_s }
+      format.json { render json: { id: @poem.digest, contents: @poem.contents } }
+      format.jpeg { send_data poem_preview_data(@poem), type: "image/jpeg", disposition: "inline" }
     end
   end
 
