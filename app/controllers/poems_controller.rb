c2676a9a96e1f93c895db39c7f307b7f67552902
Daniel Bogan <d@waferbaby.com>
2025-07-06 07:05:44 +0000

    Add support for the .jpg output

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -6,5 +6,23 @@ class PoemsController < ApplicationController
 
   def show
     @poem = Poem.find_by!(digest: params[:id])
+
+    respond_to do |format|
+      format.html { }
+      format.jpeg do
+        send_data poem_preview_data(@poem), type: "image/jpeg", disposition: "inline"
+      end
+    end
+  end
+
+  private
+
+  def poem_preview_data(poem)
+    return nil unless poem.preview.attached?
+
+    path = ActiveStorage::Blob.service.path_for(poem.preview.key)
+    return nil unless path.present? && File.exist?(path)
+
+    File.read(path)
   end
 end
