92a2bcceb59e675469c70c29e7c1c0e2cf21f450
Daniel Bogan <d@waferbaby.com>
2025-07-06 07:05:26 +0000

    Generate the preview image and attach it on create

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,5 +1,11 @@
 class Poem < ApplicationRecord
+  PREVIEW_OFFSET = 100
+
+  has_one_attached :preview
+
   enum :form, [ :haiku, :limerick ]
+
+  after_create :generate_preview
 
   def self.generate(form = :haiku)
     options = { proper: false }
@@ -24,5 +30,27 @@ class Poem < ApplicationRecord
 
   def to_s
     contents.downcase
+  end
+
+  private
+
+  def generate_preview
+    text = Vips::Image.text(
+      self.to_s,
+      font: "alice",
+      fontfile: Rails.root.join("app", "assets", "fonts", "alice.ttf").to_s,
+      dpi: 300
+    )
+
+    return nil unless text.present?
+
+    image = Vips::Image.black(text.width + PREVIEW_OFFSET, text.height + PREVIEW_OFFSET).cast("uchar").copy(interpretation: :rgb)
+    return nil unless image.present?
+
+    composite = image.composite(text, "over", x: PREVIEW_OFFSET / 2, y: PREVIEW_OFFSET / 2)
+    return nil unless composite.present?
+
+    buffer = StringIO.new(composite.write_to_buffer(".jpg"))
+    self.preview.attach(io: buffer, filename: "#{digest}.jpg", content_type: "image/jpeg", identify: false)
   end
 end
