d7ef36adc59bd03f3e21f815e7f859b9d2e32f19
Daniel Bogan <d@waferbaby.com>
2025-07-13 21:56:00 +0000

    Actually generate a poem, too

--- app/jobs/post_to_bluesky_job.rb
+++ app/jobs/post_to_bluesky_job.rb
@@ -3,7 +3,7 @@ class PostToBlueskyJob < ApplicationJob
 
   def perform(*args)
     if Yonder.create_session(username: ENV["BLUESKY_USERNAME"], password: ENV["BLUESKY_PASSWORD"])
-      Yonder.create_post(user_did: Yonder.session.user_did, message: poem.contents)
+      Yonder.create_post(user_did: Yonder.session.user_did, message: Poem.generate.contents)
     end
   rescue Yonder::Error => e
     Rails.logger.error("Failed to post to Bluesky: #{e}")
