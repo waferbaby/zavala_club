fd039991d3abe013bcc84dfce83c27d7b85db839
Daniel Bogan <d@waferbaby.com>
2025-07-13 21:43:49 +0000

    Simplify the logic

--- app/jobs/post_to_bluesky_job.rb
+++ app/jobs/post_to_bluesky_job.rb
@@ -1,24 +1,9 @@
 class PostToBlueskyJob < ApplicationJob
   queue_as :default
 
-  def perform(poem_id: nil)
-    session_data = Rails.cache.fetch(:zavala_club_bluesky_session) do
-      Yonder.create_session(username: ENV["BLUESKY_USERNAME"], password: ENV["BLUESKY_PASSWORD"])
-      Marshal.dump(Yonder.session)
-    end
-
-    Yonder.session = Marshal.load(session_data)
-    poem = poem_id ? Poem.find(poem_id) : Poem.generate
-
-    begin
+  def perform(*args)
+    if Yonder.create_session(username: ENV["BLUESKY_USERNAME"], password: ENV["BLUESKY_PASSWORD"])
       Yonder.create_post(user_did: Yonder.session.user_did, message: poem.contents)
-    rescue Yonder::TokenExpiredError => e
-      Rails.logger.warn("Auth token expired - requesting new tokens")
-
-      if Yonder.refresh_session
-        Rails.cache.write(:zavala_club_bluesky_session, Marshal.dump(Yonder.session))
-        PostToBlueskyJob.perform_later(poem_id: poem.id)
-      end
     end
   rescue Yonder::Error => e
     Rails.logger.error("Failed to post to Bluesky: #{e}")
